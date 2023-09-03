b805df575ad61b5285f96d486dcf85271cedaf63
Chaotic Grumpy <imon086@gmail.com>
2023-09-04 01:36:39 +0400

    add leap/fzf/undotree

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,4 @@
 return {
-  { "rose-pine/neovim", name = "rose-pine", opts = { variant = "dawn", disable_italics = true } },
   {
     "navarasu/onedark.nvim",
     opts = {
@@ -7,21 +6,9 @@ return {
     },
   },
   {
-    "projekt0n/github-nvim-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("github-theme").setup({
-        -- ...
-      })
-
-      -- vim.cmd('colorscheme github_dark')
-    end,
-  },
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github_light",
+      colorscheme = "onedark",
     },
   },
 }
